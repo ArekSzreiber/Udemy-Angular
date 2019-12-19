0ea9ce791d5bf9c2a1c52a2d8c0dc87b98fe567b
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2019-12-19 23:20:22 +0100

    41. [OPTIONAL] Assignment Solution

---
+++ src/app/display-details/display-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-display-details',
+  templateUrl: './display-details.component.html',
+  styleUrls: ['./display-details.component.css']
+})
+export class DisplayDetailsComponent implements OnInit {
+  displayParagraph: boolean = true;
+  clicks = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onToggleDisplay() {
+    this.displayParagraph = !this.displayParagraph;
+    this.clicks.push(this.clicks.length+1);
+  }
+
+}
